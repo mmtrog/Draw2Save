96f9ac6af0d19dfadeccd0f6e90f13ff113546c4
mmtrog <minhtrong2k@gmail.com>
2022-08-29 14:54:29 +0700

    Changed Line Generator - Line cut when touch Platform

--- Assets/Script/Line/LineGenerator.cs
+++ Assets/Script/Line/LineGenerator.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6ad73460b31da3045ad7a417a764e2ca6ac6542e752be6c368800236561ff34b
-size 7424
+oid sha256:d1ed8c8b2854a41b3d94027b12f66feb29e5ede205b0ef0cba9fd71bf0dba904
+size 7995
