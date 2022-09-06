c611332c85dc6c333cbacc87e249bdc71008fbca
mmtrog <minhtrong2k@gmail.com>
2022-09-06 15:13:30 +0700

    Fixed some bugs

--- Assets/Script/Game Manager/GameManager.cs
+++ Assets/Script/Game Manager/GameManager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f06ee6d4c35d45e9a145f4e6b6f13c1591753f0e3052f63790d6d4a42280ed3e
-size 4423
+oid sha256:9c404cfe23e72d5ac83be65dbfb4460c75da664292da20241bacdcd5f8bf702f
+size 4406
